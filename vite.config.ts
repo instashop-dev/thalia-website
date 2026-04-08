035fe7bf60045c9a63b99ea0c9b330c5d85252a1
sweta Gupta <sweta@thaliatechnologies.com>
2026-04-08 12:53:13 +0530

    Update dependencies and configuration
    - Upgraded "@vitejs/plugin-react-swc" to version 4.3.0.
    - Updated "vite" to version 8.0.7.
    - Added new optional dependencies for "@emnapi/core", "@emnapi/runtime", and "@emnapi/wasi-threads".
    - Updated various "@esbuild" packages to version 0.27.7 and changed the required Node.js version to >=18 in their configurations.
    - Configured Vite server to allow specific hosts for development.

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["determinate-frontierless-samara.ngrok-free.dev"],
     hmr: {
       overlay: false,
     },
